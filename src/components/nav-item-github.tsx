4d1ab4efcd2e1dc9f3b8771cb966a9c71adb1ea6
MUKE JOHNBAPTIST <64189841+MUKE-coder@users.noreply.github.com>
2025-08-20 15:41:03 +0300

    final

--- src/components/nav-item-github.tsx
+++ src/components/nav-item-github.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -8,9 +9,18 @@ import { Icons } from "./icons";
 export function NavItemGitHub() {
   return (
     <Button variant="outline" size="icon" asChild>
-      <a href={SOURCE_CODE_GITHUB_URL} target="_blank" rel="noopener">
-        <Icons.github />
-        <span className="sr-only">GitHub</span>
+      <a
+        href={SOURCE_CODE_GITHUB_URL}
+        target="_blank"
+        rel="noopener"
+        className="block"
+      >
+        <img
+          className="h-auto w-32 rounded-full"
+          alt="yOUTUBE"
+          src="/youtube.webp"
+          fetchPriority="high"
+        />
       </a>
     </Button>
   );
