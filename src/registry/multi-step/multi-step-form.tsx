3819921ade756467255b596c32bbf19fcfc3adf4
MUKE JOHNBAPTIST <64189841+MUKE-coder@users.noreply.github.com>
2025-09-09 16:58:17 +0300

    added

--- src/registry/multi-step/multi-step-form.tsx
+++ src/registry/multi-step/multi-step-form.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function MultiStepForm() {
   return (
     <div>
+      <h1>Install</h1>
       <Button>Multi Step Form</Button>
     </div>
   );
